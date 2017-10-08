44ffe369785ad6f585e95c5f7cbb838260fc4b84
Kavya-Venkat <kavya23123@gmail.com>
2017-10-08 11:21:48 +0530

    new app changes

--- src/app/main.js
+++ src/app/main.js
@@ -1,5 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
 import { AppModule } from './app.module';
 //to bootstrap and run your app form appmodule
 platformBrowserDynamic().bootstrapModule(AppModule);
+//# sourceMappingURL=main.js.map
